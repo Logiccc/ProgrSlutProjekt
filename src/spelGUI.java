c02d6a7c76a80c89d8f4c81fb53e786ea124059c
David Persson <te16.david.persson@umea.nti.se>
2019-05-03 12:31:12 +0200

    Fixade databas connectionen med java genom att kopiera och klistrade in den igen från övningsppgiften. Även gjort en början på ett GUi till spelet, men den nuvarande versionen saknar funktionallitet.

---
+++ src/spelGUI.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class spelGUI {
+    private JPanel gui;
+    private JTextArea textArea1;
+    private JTextPane textPane1;
+    private JTextPane textPane2;
+    private JButton button1;
+    private JButton button2;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Pasta Alfredos Äventyr");
+        spelGUI test = new spelGUI();
+        frame.setContentPane(test.gui);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(new Dimension(800, 600));
+        frame.setVisible(true);
+
+    }
+}
